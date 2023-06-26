ba722b93f52e52b70d77ca44b2de8d45e5c653d2
Vidit Jain <96811167+Vidit-Jain2206@users.noreply.github.com>
2023-06-26 14:58:27 +0530

    project added

--- myhome/src/App.js
+++ myhome/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import Signup from "./pages/Signup";
+import Offers from "./pages/Offers";
+import ForgetPassword from "./pages/ForgetPassword";
+import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
+import Listing from "./pages/Listing";
+import Category from "./pages/Category";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />}></Route>
+          </Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
+          <Route path="/sign-up" element={<Signup />}></Route>
+          <Route path="/create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />}></Route>
+          </Route>
+          <Route path="/edit-listing" element={<PrivateRoute />}>
+            <Route
+              path="/edit-listing/:listingId"
+              element={<EditListing />}
+            ></Route>
+          </Route>
+          <Route path="/offers" element={<Offers />}></Route>
+          <Route path="/category/:type" element={<Category />}></Route>
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          ></Route>
+          <Route path="/forgot-password" element={<ForgetPassword />}></Route>
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </>
   );
 }
 
